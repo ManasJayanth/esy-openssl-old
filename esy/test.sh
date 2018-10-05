0443fe3396caf6bdeeb12f5e5166c57bffaab896
Bryan Phelps <bryphe@outlook.com>
2018-10-05 16:42:13 -0700

    Add test program

---
+++ esy/test.sh
@@ -0,0 +1,23 @@
+pwd
+mkdir _test
+cd _test
+
+INCLUDE=$cur__install/include
+
+if which x86_64-w64-mingw32-gcc; then
+    CC=x86_64-w64-mingw32-gcc
+else
+    CC=gcc
+fi
+
+echo "Using compiler: $CC"
+
+echo "include..."
+ls -a $INCLUDE
+echo "lib.."
+ls -a $cur__lib
+$CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lssl -lcrypto
+
+cp $cur__bin/*.dll .
+
+./test.exe
