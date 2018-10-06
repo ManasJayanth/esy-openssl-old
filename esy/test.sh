153cac57e3673958d59d18c2f0ef5658beaef1f6
Bryan Phelps <bryphe@outlook.com>
2018-10-05 17:56:41 -0700

    Add LD_LIBRARY_PATH

--- esy/test.sh
+++ esy/test.sh
@@ -20,6 +20,7 @@ ls -a $cur__lib
 $CC ./../esy/test.c -o ./test.exe -I$INCLUDE -L$cur__lib -lssl -lcrypto
 
 export PATH=$PATH:$cur__bin:$cur__lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cur__lib
 
 echo "Augmented path: $PATH"
 
